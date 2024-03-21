39572d04ff17b3466f58039a8b611a29a51e4f9c
Волоснов Егор <139017811+egorvolosnov@users.noreply.github.com>
2024-03-21 15:48:27 +0300

    Создание новой ветки для разработки экрана с настройками

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreenState extends StatefulWidget {
+  const SettingsScreenState({super.key});
+
+  @override
+  State<SettingsScreenState> createState() => __SettingsScreenStateState();
+}
+
+class __SettingsScreenStateState extends State<SettingsScreenState> {
+  @override 
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
